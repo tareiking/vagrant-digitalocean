1c6a13636b517a5ba4b8e777ac73a2215af79c67
Bronson Quick <bronson@sennza.com.au>
2014-05-12 19:58:17 +1000

    Setting up Bundle

---
+++ vagrant-chassis-digitalocean/lib/vagrant/chassis/digitalocean.rb
@@ -0,0 +1,9 @@
+require "vagrant/chassis/digitalocean/version"
+
+module Vagrant
+  module Chassis
+    module Digitalocean
+      # Your code goes here...
+    end
+  end
+end
---
+++ vagrant-chassis-digitalocean/lib/vagrant/chassis/digitalocean/version.rb
@@ -0,0 +1,7 @@
+module Vagrant
+  module Chassis
+    module Digitalocean
+      VERSION = "0.0.1"
+    end
+  end
+end
